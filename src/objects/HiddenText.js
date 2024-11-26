60ccb00af9f6158287153325b3de89ff8c4b7ac1
Grink <40912659+jonathan-rose@users.noreply.github.com>
2024-11-26 21:21:22 +0000

    Update src/objects/HiddenText.js
    Co-authored-by: Dave Kimber <lordkimber@gmail.com>

--- src/objects/HiddenText.js
+++ src/objects/HiddenText.js
@@ -11,17 +11,12 @@ export default class HiddenText extends Phaser.GameObjects.Layer {
 
         this.hiddenText = [ text1 ];
 
-        this.graphics = this.scene.make.graphics();
+        this.graphics = this.scene.make.graphics({
+            x: this.lightSource.x,
+            y: this.lightSource.y
+        });
         this.graphics.fillStyle(0xffffff);
-        // For some reason directly setting the starting location
-        // of the mask in the fillCircle() method call causes
-        // the circle to be offset from light source, so
-        // you have to use setPosition directly after.
-        // This is super annoying, especially as they both report
-        // having the same (x, y) co-ordinates but
-        // I don't have time to work out why right now.
         this.graphics.fillCircle(0, 0, this.castRadius);
-        this.graphics.setPosition(this.lightSource.x, this.lightSource.y);
 
         const mask = this.graphics.createGeometryMask();
         this.setMask(mask);
