d91e789cecc9abaf5504a18ad9c696488ff77faf
Beth <bethcott@hotmail.co.uk>
2024-11-27 13:35:51 +0000

    Lines are drawn between stars when clicked

--- src/scenes/Telescope.js
+++ src/scenes/Telescope.js
@@ -11,17 +11,39 @@ export class Telescope extends Scene
     create ()
     {
         this.add.image(512, 384, 'starfield');
-        this.add.image(512, 384, 'star').setInteractive()
+
+        this.prevX = 0;
+        this.prevY = 0;
+
+        this.star1 = this.add.image(512, 384, 'star').setInteractive()
         .on('pointerdown', () => {
-            this.add.image(512,384, 'starGlow')
+            this.add.image(512,384, 'starGlow');
+            this.DrawLine(this.star1.x, this.star1.y);
+            // this.prevX = this.star1.x;
+            // this.prevY = this.star1.y;
         });;
 
-        this.stars = this.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 200, y: 300, stepX: 70 }
-        });
+        this.star2 = this.add.image(600, 300, 'star').setInteractive()
+        .on('pointerdown', () => this.DrawLine(this.star2.x, this.star2.y));
 
+        this.star3 = this.add.image(700, 400, 'star').setInteractive()
+        .on('pointerdown', () => this.DrawLine(this.star3.x, this.star3.y));
+
+        this.star4 = this.add.image(800, 350, 'star').setInteractive()
+        .on('pointerdown', () => this.DrawLine(this.star4.x, this.star4.y));
+
+        // this.star1.on(
+        //     'pointerdown',
+        //     () => this.add.line(
+        //         0,
+        //         0,
+        //         this.star1.x,
+        //         this.star1.y,
+        //         this.star2.x,
+        //         this.star2.y,
+        //         0xff0000
+        //     ).setOrigin(0, 0)
+        // );
 
         this.add.text(20, 20, 'Back', {
             fontFamily: 'Arial Black', fontSize: 64, color: Util.colours.WHITE,
@@ -29,9 +51,27 @@ export class Telescope extends Scene
             align: 'center'
         }).setInteractive()
         .on('pointerdown', () => {
-
             this.scene.start('Game');
-
         });
+    }
+
+    DrawLine(x, y) {
+        if(this.prevX == 0)
+        {
+            this.prevX = x;
+            this.prevY = y;
+            return;
+        }
+        this.add.line(
+            0,
+            0,
+            this.prevX,
+            this.prevY,
+            x,
+            y,
+            0xff0000
+        ).setOrigin(0, 0);
+        this.prevX = x;
+        this.prevY = y;
     }
 }
